bc6b737aba5e536ae072d6dcc148bf71d937cc7f
Sreeleka <sreelekamanickam@gmail.com>
2026-05-29 15:43:10 +0530

    Enable sample loader

--- review/apps.py
+++ review/apps.py
@@ -1,9 +1,24 @@
 from django.apps import AppConfig
 
+
 class ReviewConfig(AppConfig):
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "review"
 
     def ready(self):
-        pass
+
+        try:
+
+            from ingestion.sample_loader import (
+                load_sample_data
+            )
+
+            load_sample_data()
+
+        except Exception as e:
+
+            print(
+                "LOADER ERROR:",
+                e
+            )

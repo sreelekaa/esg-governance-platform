21e13588ea6d29c6370287f9eb4ddf9c234fee6e
Sreeleka <sreelekamanickam@gmail.com>
2026-05-29 14:20:15 +0530

    Add startup logs

--- review/apps.py
+++ review/apps.py
@@ -1,21 +1,24 @@
-
 from django.apps import AppConfig
 
 class ReviewConfig(AppConfig):
 
     default_auto_field = "django.db.models.BigAutoField"
-
     name = "review"
 
     def ready(self):
 
+        print("REVIEW APP STARTED")
+
         try:
 
-            from ingestion.sample_loader import (
-                load_sample_data
-            )
+            from ingestion.sample_loader import load_sample_data
+
+            print("CALLING SAMPLE LOADER")
 
             load_sample_data()
 
+            print("SAMPLE LOADER FINISHED")
+
         except Exception as e:
-            print(e)
+
+            print("LOADER ERROR:", e)
